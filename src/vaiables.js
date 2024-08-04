28f85ae3d0f0205fae5ba8b2c4a93fc490529fb1
MwangiKariuki586 <mwangikariuki586@gmail.com>
2024-08-04 21:30:30 +0300

    update

--- src/vaiables.js
+++ src/vaiables.js
@@ -1,8 +1,20 @@
-export const aboutinfo = "http://localhost:3500/about";
+// export const aboutinfo = "http://localhost:3500/about";
+// export const frontendexperienceinfo =
+//   "http://localhost:3500/frontend_experience";
+// export const backendexperienceinfo = "http://localhost:3500/backend_experience";
+// export const databasesexperienceinfo =
+//   "http://localhost:3500/database_experience";
+// export const staticfiles = "http://localhost:3500/";
+// export const projectsinfo = "http://localhost:3500/projects";
+export const aboutinfo =
+  "https://personal-portfolio-backend-express-mongodb.vercel.app/about";
 export const frontendexperienceinfo =
-  "http://localhost:3500/frontend_experience";
-export const backendexperienceinfo = "http://localhost:3500/backend_experience";
+  "https://personal-portfolio-backend-express-mongodb.vercel.app/frontend_experience";
+export const backendexperienceinfo =
+  "https://personal-portfolio-backend-express-mongodb.vercel.app/backend_experience";
 export const databasesexperienceinfo =
-  "http://localhost:3500/database_experience";
-export const staticfiles = "http://localhost:3500/";
-export const projectsinfo = "http://localhost:3500/projects";
+  "https://personal-portfolio-backend-express-mongodb.vercel.app/database_experience";
+export const staticfiles =
+  "https://personal-portfolio-backend-express-mongodb.vercel.app/";
+export const projectsinfo =
+  "https://personal-portfolio-backend-express-mongodb.vercel.app/projects";
